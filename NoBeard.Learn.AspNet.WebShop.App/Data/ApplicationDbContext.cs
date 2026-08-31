40e9ff601245ca19a995f1d91947c6ebfd32b49c
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-08-31 20:18:58 +0200

    Add project files.

---
+++ NoBeard.Learn.AspNet.WebShop.App/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NoBeard.Learn.AspNet.WebShop.App.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ NoBeard.Learn.AspNet.WebShop.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NoBeard.Learn.AspNet.WebShop.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
