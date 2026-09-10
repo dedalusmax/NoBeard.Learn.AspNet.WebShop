c636a7191c35db344e01e376e50afdd16dbf55c9
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-09-10 19:01:31 +0200

    list

---
+++ NoBeard.Learn.AspNet.WebShop.App/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NoBeard.Learn.AspNet.WebShop.App.Data;
+
+namespace NoBeard.Learn.AspNet.WebShop.App.Controllers;
+
+public class ProductsController(ApplicationDbContext context) : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Categories"] = new SelectList(context.Categories, "Id", "Name");
+
+        var products = context.Products.ToList();
+
+        return View(products);
+    }
+}
