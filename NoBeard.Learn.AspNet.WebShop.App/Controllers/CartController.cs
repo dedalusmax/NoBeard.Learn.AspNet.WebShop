665275a522ed17aac3531267175570d96d0c8c5c
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-09-14 19:49:30 +0200

    session

--- NoBeard.Learn.AspNet.WebShop.App/Controllers/CartController.cs
+++ NoBeard.Learn.AspNet.WebShop.App/Controllers/CartController.cs
@@ -1,15 +1,17 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NoBeard.Learn.AspNet.WebShop.App.Data;
+using NoBeard.Learn.AspNet.WebShop.App.Extensions;
 using NoBeard.Learn.AspNet.WebShop.App.Models;
 
 namespace NoBeard.Learn.AspNet.WebShop.App.Controllers;
 
 [Authorize]
-public class CartController : Controller
+public class CartController(ApplicationDbContext context) : Controller
 {
     public IActionResult Index()
     {
-        var cart = new Cart();
+        var cart = HttpContext.Session.GetCart();
 
         return View(cart);
     }
@@ -17,6 +19,39 @@ public class CartController : Controller
     [HttpPost]
     public IActionResult AddToCart(int productId)
     {
+        var cart = HttpContext.Session.GetCart();
+
+        if (cart.Items.Count == 0)
+        {
+            var item = new CartItem
+            {
+                Product = context.Products.Find(productId)!,
+                Quantity = 1
+            };
+
+            cart.Items.Add(item);
+        }
+        else
+        {
+            if (cart.Items.Any(x => x.Product.Id == productId))
+            {
+                var item = cart.Items.Single(x => x.Product.Id == productId);
+                item.Quantity++;
+            }
+            else
+            {
+                var item = new CartItem
+                {
+                    Product = context.Products.Find(productId)!,
+                    Quantity = 1
+                };
+
+                cart.Items.Add(item);
+            }
+        }
+
+        HttpContext.Session.SetCart(cart);
+
         return RedirectToAction(nameof(Index));
     }
 }
