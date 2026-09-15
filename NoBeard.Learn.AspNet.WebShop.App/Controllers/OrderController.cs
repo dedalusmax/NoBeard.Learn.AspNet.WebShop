3c15649085f0792f4f12ca129d28d9b63ebdbd20
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-09-15 19:05:36 +0200

    index view

---
+++ NoBeard.Learn.AspNet.WebShop.App/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoBeard.Learn.AspNet.WebShop.App.Extensions;
+
+namespace NoBeard.Learn.AspNet.WebShop.App.Controllers;
+
+public class OrderController : Controller
+{
+    public IActionResult Index()
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        ViewData["Cart"] = cart;
+
+        return View();
+    }
+}
