ffa634164af0ddb4a72acc86c43b376c63c0d72b
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-09-15 19:49:39 +0200

    success

--- NoBeard.Learn.AspNet.WebShop.App/Controllers/OrderController.cs
+++ NoBeard.Learn.AspNet.WebShop.App/Controllers/OrderController.cs
@@ -1,15 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NoBeard.Learn.AspNet.WebShop.App.Extensions;
+using NoBeard.Learn.AspNet.WebShop.App.Models;
 
 namespace NoBeard.Learn.AspNet.WebShop.App.Controllers;
 
 public class OrderController : Controller
 {
-    public IActionResult Index()
+    public IActionResult Index(bool? success)
     {
+        ViewData["Success"] = success;
+
         var cart = HttpContext.Session.GetCart();
 
         ViewData["Cart"] = cart;
+
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("CustomerFirstName,CustomerLastName,CustomerEmailAddress,CustomerPhoneNumber,CustomerAddress")] Order order)
+    {
+        ModelState.Remove("Items");
+
+        if (ModelState.IsValid)
+        {
+
+
+
+
+            return RedirectToAction(nameof(Index), new { success = true });
+        }
 
         return View();
     }
