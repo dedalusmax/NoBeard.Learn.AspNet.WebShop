bba58a1be5d26bdd98d3da84680fb60523e7e377
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-09-17 19:45:51 +0200

    view comp

--- NoBeard.Learn.AspNet.WebShop.App/Controllers/OrderController.cs
+++ NoBeard.Learn.AspNet.WebShop.App/Controllers/OrderController.cs
@@ -72,4 +72,21 @@ public class OrderController(ApplicationDbContext context) : Controller
 
         return View();
     }
+
+    [HttpPost]
+    public IActionResult RemoveFromCart(int productId)
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        if (cart.Items.Any(x => x.Product.Id == productId))
+        {
+            var item = cart.Items.Single(x => x.Product.Id == productId);
+            cart.Items.Remove(item);
+        }
+
+        HttpContext.Session.SetCart(cart);
+
+        return RedirectToAction(nameof(Index));
+    }
+
 }
---
+++ NoBeard.Learn.AspNet.WebShop.App/Views/Shared/Components/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NoBeard.Learn.AspNet.WebShop.App.Extensions;
+
+namespace NoBeard.Learn.AspNet.WebShop.App.Views.Shared.Components.ShoppingCart;
+
+//[ViewComponent(Name = "Cart")]
+public class ShoppingCart : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var cart = HttpContext.Session.GetCart();
+
+        return View(cart);
+    }
+}
