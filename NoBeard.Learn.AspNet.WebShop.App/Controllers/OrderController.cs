1a0e7f5d4039d038c1abc58a176166ccf55f4a72
Ratko Ćosić <ratko_cosic@hotmail.com>
2026-09-21 19:00:49 +0200

    order save

--- NoBeard.Learn.AspNet.WebShop.App/Controllers/OrderController.cs
+++ NoBeard.Learn.AspNet.WebShop.App/Controllers/OrderController.cs
@@ -1,21 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using NoBeard.Learn.AspNet.WebShop.App.Data;
 using NoBeard.Learn.AspNet.WebShop.App.Extensions;
 using NoBeard.Learn.AspNet.WebShop.App.Models;
+using System.Security.Claims;
 
 namespace NoBeard.Learn.AspNet.WebShop.App.Controllers;
 
+[Authorize]
 public class OrderController(ApplicationDbContext context) : Controller
 {
     public IActionResult Index(bool? success)
     {
         ViewData["Success"] = success;
 
+        //var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        //var user = context.Users.Find(userId);
+
+        //var userEmail = HttpContext.User.Identity.Name; // name = email ??
+        //var user = context.Users.FirstOrDefault(u => u.Email == userEmail);
+
+        var order = new Order();
+
+        var user = GetUser();
+        if (user != null)
+        {
+            order.CustomerFirstName = string.IsNullOrEmpty(user.FirstName) ? string.Empty : user.FirstName;
+            order.CustomerLastName = string.IsNullOrEmpty(user.LastName) ? string.Empty : user.LastName;
+            order.CustomerEmailAddress = string.IsNullOrEmpty(user.Email) ? string.Empty : user.Email;
+            order.CustomerPhoneNumber = string.IsNullOrEmpty(user.PhoneNumber) ? string.Empty : user.PhoneNumber;
+            order.CustomerAddress = string.IsNullOrEmpty(user.Address) ? string.Empty : user.Address;
+        }
+
         var cart = HttpContext.Session.GetCart();
 
         ViewData["Cart"] = cart;
 
-        return View();
+        return View(order);
+    }
+
+    private ApplicationUser? GetUser()
+    {
+        var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        return context.Users.Find(userId);
     }
 
     [HttpPost]
@@ -23,6 +50,8 @@ public class OrderController(ApplicationDbContext context) : Controller
     public IActionResult Create([Bind("CustomerFirstName,CustomerLastName,CustomerEmailAddress,CustomerPhoneNumber,CustomerAddress")] Order order)
     {
         ModelState.Remove("Items");
+        ModelState.Remove("UserId");
+        ModelState.Remove("User");
 
         if (ModelState.IsValid)
         {
@@ -35,6 +64,8 @@ public class OrderController(ApplicationDbContext context) : Controller
             try
             {              
                 order.Total = cart.GrandTotal;
+                order.DateTimeCreated = DateTime.UtcNow;
+                order.UserId = GetUser()!.Id;
 
                 context.Orders.Add(order);
                 context.SaveChanges();
@@ -69,8 +100,20 @@ public class OrderController(ApplicationDbContext context) : Controller
 
             return RedirectToAction(nameof(Index), new { success = true });
         }
+        else
+        {
+            var errors = new List<string>();
+            foreach (var modelState in ModelState.Values)
+            {
+                foreach (var error in modelState.Errors)
+                {
+                    errors.Add(error.ErrorMessage);
+                }
+            }
 
-        return View();
+            //return BadRequest(new { errors });
+            return RedirectToAction(nameof(Index), new { success = false, errors });
+        }
     }
 
     [HttpPost]
